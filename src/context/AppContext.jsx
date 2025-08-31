cb4356d8e024227fed438518d3c59212db406c02
SithijaSankalpa <kshithijasankalpa1234@gmail.com>
2025-08-31 20:25:10 +0530

    Initial commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+
+const AppContext = createContext()
+
+export const AppContextProvider = ({ children }) => {
+
+  const value = {}
+
+  return (  
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useAppContext = () => useContext(AppContext)
+
