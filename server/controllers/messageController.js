2779180e58d44dad63b6d47ced4f2d7977b5d019
SithijaSankalpa <kshithijasankalpa1234@gmail.com>
2025-09-12 20:48:23 +0530

    created functions for credits and transactions

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -2,6 +2,7 @@ import axios from "axios"
 import Chat from "../models/Chat.js"
 import User from "../models/User.js";
 import imagekit from "../configs/imageKit.js";
+import openai from "../configs/openai.js";
 
 // Text-based AI chat message controller
 export const textMessageController = async (req, res) =>{
@@ -16,7 +17,7 @@ export const textMessageController = async (req, res) =>{
     const {chatId, prompt} = req.body
     
     const chat = await Chat.findOne({userId, _id: chatId})
-    chat.messages.push({role: "user", content: prompt, timestamp: Date.now,isImage: false})
+    chat.messages.push({role: "user", content: prompt, timestamp: Date.now(),isImage: false})
 
     const { choices } = await openai.chat.completions.create({
     model: "gemini-2.0-flash",
@@ -28,7 +29,7 @@ export const textMessageController = async (req, res) =>{
      ],
     });
     
-    const reply = {...choices[0].message, timestamp: Date.now,isImage: false }
+    const reply = {...choices[0].message, timestamp: Date.now(),isImage: false }
       res.json({success: true, reply})
     chat.messages.push(reply)
     await chat.save()
@@ -57,7 +58,7 @@ export const imageMessageController = async (req, res) => {
     chat.messages.push({
       role: "user", 
       content: prompt, 
-      timestamp: Date.now,
+      timestamp: Date.now(),
       isImage: false
     });
 
@@ -82,7 +83,7 @@ export const imageMessageController = async (req, res) => {
     const reply = {
       role: 'assistant', 
       content: uploadResponse.url,
-      timestamp: Date.now,
+      timestamp: Date.now(),
       isImage: true,
       isPublished
     }
