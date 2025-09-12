3484ebde878a19f5ac1fc1e5f9656521b4ef88e6
SithijaSankalpa <kshithijasankalpa1234@gmail.com>
2025-09-12 22:40:13 +0530

    intergrated the payment gateway and add server live

--- server/routes/creditRoutes.js
+++ server/routes/creditRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import { getPlans, purchasePlan } from '../controllers/creditController.js';
+import { protect } from '../middlewares/auth.js';
 
 const creditRouter = express.Router()
 
